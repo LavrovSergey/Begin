c9a2ec0e2f779f5e364d9e17661559c628ff7de3
LavrovSergey <54863977+LavrovSergey@users.noreply.github.com>
2019-09-18 18:36:44 +0300

    Create Begin1.cpp

---
+++ Begin1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Enter the length of the side of the square \n";
+	int a;
+		cin >> a;
+		int s = a * a;
+	cout << s;
+
+}
+
+
+
+
